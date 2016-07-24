8177992b3a25e48e30f6503624d0657df60b5798
simon <simon@DESKTOP-LKEG83G>
2016-07-24 17:43:35 +0200

    Add project files.

---
+++ GTAGOAlpha/HttpClientDefaultAdapter.cs
@@ -0,0 +1,14 @@
+﻿using Org.Apache.Http.Client;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PokeAPI
+{
+    public class HttpClientDefaultAdapter : IHttpClientAdapter
+    {
+
+        public Task<Stream> GetStreamAsync(Uri    requestUri) => GetStreamAsync(requestUri);
+        public Task<string> GetStringAsync(string requestUri) => GetStringAsync(requestUri);
+    }
+}
